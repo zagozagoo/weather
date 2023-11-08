9581f69b3536a9e084cb75c4427a87c48aa7865b
zagozagoo <vivizago4884@gmail.com>
2023-11-08 10:56:58 -0300

    commit

---
+++ menu.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    char line[200];
+    char date[120];
+    int year, month, day;
+
+    file = fopen ("weather_data.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo de dados metereologicos.\n");
+        return 1;
+    }
+
+    printf("Insira ano, mes e dia, respectivamente, para visualizar a previsao:\n");
+    scanf("%d %d %d", &year, &month, &day);
+    printf("Previsao para a data %d-%02d-%02d:\n", year, month, day);
+
+    while (fgets(line, sizeof(line), file) != NULL) /*le cada linha do arqv, fgets le a proxima linha do arqv e armazena na variavel*/
+    {
+        if (sscanf(line, "%*s%19s", date) == 1)
+        {
+            int line_year = year, line_month = month, line_day = day;
+            if (sscanf(date, "%d %d %d", &line_year, &line_month, &line_day) == 3) /*Verifica se o formato de data esta certo, 3 verifica se sscanf conseguiu atribuir com sucesso 3 valores inteiros às variaveis*/
+            {
+                printf(line);
+                while (fgets(line, sizeof(line), file) && strcmp(line, "\n") != 0)
+                {
+                    printf("%s", line);
+                }
+                break;
+            }
+        }
+        else
+        {
+            printf("Opcao invalida. Escolha 1 para visualizar previsao por data ou 2 para sair.\n");
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+/*if (sscanf(line, "%*s %19s", date) == 1) {: Dentro do loop, esta linha verifica se a linha atual pode ser analisada usando sscanf.
+O formato especificado "%*s %19s" é usado para analisar a linha. O %*s ignora a primeira palavra na linha, e
+%19s lê e armazena a segunda palavra na variável date. O retorno de sscanf (1 neste caso) indica que a linha foi analisada com sucesso*/
+
+
--- weatherapi.py
+++ weatherapi.py
@@ -1,6 +1,6 @@
 import requests, json
 
-api_key = "your_api_here" 
+api_key = "api" 
  
 city_name = input("Enter city name : ")
 
@@ -14,20 +14,20 @@ base_url = "https://api.openweathermap.org/data/2.5/forecast?lat="
 
 # Concatena todas as informações em um link(as informações do json estão sendo convertidas 
 # para string para poderem ser concatenadas)
-histweather = base_url + str(geojson["lat"]) + "&lon=" + str(geojson["lon"]) + "&appid=" + api_key
+histweather = base_url + str(geojson["lat"]) + "&lon=" + str(geojson["lon"]) + "&appid=" + api_key + "&units=metric"
 histresponse = requests.get(histweather)
 histjson = histresponse.json()
 
 with open ("weather_data.txt", "w") as file:
   
 # Percorre a lista dentro do json que possui todas as informações de previsão dos proximos 5 dias
-for i in range(len(histjson["list"])): 
-    list_data = histjson["list"][i] # Valor atual da lista
-    main_info = list_data["main"] # Pegando os dados principais do valor atual
-  
-    file.write(f"{list['dt_txt']}:") # Printando a data exata daquelas informações
-    file.write(f"Temperatura: {main_info['temp']}")
-    file.write(f"Humidade: {main_info['humidity']}")
-    file.write(f"Pressao: {main_info['pressure']}\n\n")
+    for i in range(len(histjson["list"])): 
+        list_data = histjson["list"][i] # Valor atual da lista
+        main_info = list_data["main"] # Pegando os dados principais do valor atual
+    
+        file.write(f"{list_data['dt_txt']}:") # Printando a data exata daquelas informações
+        file.write(f"Temperatura: {main_info['temp']}\n")
+        file.write(f"Humidade: {main_info['humidity']}\n")
+        file.write(f"Pressao: {main_info['pressure']}\n\n")
 
 print("Dados metereologicos foram salvos no arquivo 'weather_data.txt'")
