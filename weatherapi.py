fef5cba0ecb4f23b86429ee62117b87626db1fe1
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-08 19:57:30 -0300

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -1,33 +1,50 @@
-import requests, json
+#include <stdio.h>
+#include <string.h>
 
-api_key = "api" 
- 
-city_name = input("Enter city name : ")
+int main()
+{
+    FILE *file;
+    char line[200];
+    char date[20];
+    int year, month, day;
 
-# Link da API que pega as coordenadas geográficas do local especificado (Documentação: https://openweathermap.org/api/geocoding-api)
-geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + city_name + "&limit=1&appid=" + api_key
-georesponse = requests.get(geocoding) # Manda uma requisição para a API
-geojson = georesponse.json()[0] # Salva a resposta da API como json em uma variável
+    file = fopen("weather_data.txt", "r");
 
-# Link que pega a previsão do tempo do local especificado
-base_url = "https://api.openweathermap.org/data/2.5/forecast?lat="
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo de dados meteorologicos.\n");
+        return 1;
+    }
 
-# Concatena todas as informações em um link(as informações do json estão sendo convertidas 
-# para string para poderem ser concatenadas)
-histweather = base_url + str(geojson["lat"]) + "&lon=" + str(geojson["lon"]) + "&appid=" + api_key + "&units=metric"
-histresponse = requests.get(histweather)
-histjson = histresponse.json()
+    printf("Insira ano, mes e dia, respectivamente, para visualizar a previsao:\n");
+    scanf("%d %d %d", &year, &month, &day);
+    printf("Previsao para a data %d-%02d-%02d:\n", year, month, day);
 
-with open ("weather_data.txt", "w") as file:
-  
-# Percorre a lista dentro do json que possui todas as informações de previsão dos proximos 5 dias
-    for i in range(len(histjson["list"])): # :)
-        list_data = histjson["list"][i] # Valor atual da lista
-        main_info = list_data["main"] # Pegando os dados principais do valor atual
-    
-        file.write(f"{list_data['dt_txt']}: ") # Printando a data exata daquelas informações
-        file.write(f"Temperatura {main_info['temp']};")
-        file.write(f" Humidade: {main_info['humidity']};")
-        file.write(f" Pressao: {main_info['pressure']};\n")
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        if (sscanf(line, "%*s%19s", date))
+        {
+            int line_year, line_month, line_day;
+            if (sscanf(date, "%d %d %d", &line_year, &line_month, &line_day) == 3)
+            {
+                if (line_year == year && line_month == month && line_day == day)
+                {
+                    printf(line);
+                    while (fgets(line, sizeof(line), file) && strcmp(line, "\n") != 0)
+                    {
+                        printf("%s", line);
+                    }
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("Opcao invalida. Escolha 1 para visualizar previsao por data ou 2 para sair.\n");
+        }
+    }
 
-print("Dados metereologicos foram salvos no arquivo 'weather_data.txt'")
+    fclose(file);
+    return 0;
+}
+
